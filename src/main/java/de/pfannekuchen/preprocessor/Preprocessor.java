eef0ff97d08b469c09d2422f72174b482125fb59
Pfannekuchen <gamesupport@mgnet.work>
2021-06-19 11:32:17 +0200

    Add an Option to automatically setup an independant Fabric Project
    Just like the Forge Project this automatically sets up a Fabric Project and makes changes to it. Additionally I fixed the ability to set a custom Java Home, because Fabric requires Java 16, while Forge does not work with anything higher than 11

--- src/main/java/de/pfannekuchen/preprocessor/Preprocessor.java
+++ src/main/java/de/pfannekuchen/preprocessor/Preprocessor.java
@@ -2,34 +2,56 @@ package de.pfannekuchen.preprocessor;
 
 import java.io.File;
 
-import de.pfannekuchen.preprocessor.forge.ForgeSetup;
-import de.pfannekuchen.preprocessor.forge.ForgeSetup.Forge;
-import de.pfannekuchen.preprocessor.forge.ForgeSetup.Mappings;
 import de.pfannekuchen.preprocessor.gradle.GradleDownloader;
 import de.pfannekuchen.preprocessor.gradle.GradleSubproject;
+import de.pfannekuchen.preprocessor.loader.FabricSetup;
+import de.pfannekuchen.preprocessor.loader.FabricSetup.Fabric;
+import de.pfannekuchen.preprocessor.loader.ForgeSetup;
+import de.pfannekuchen.preprocessor.loader.ForgeSetup.Forge;
+import de.pfannekuchen.preprocessor.loader.ForgeSetup.Mappings;
 
 public class Preprocessor {
 
+	public static final String JAVA16 = "C:/Program Files/AdoptOpenJDK/jdk-16.0.1.9-hotspot/";
+	public static final String JAVA8 = "C:/Program Files/AdoptOpenJDK/jdk-8.0.275.1-openj9/";
+	
 	/** 
 	 * Method used for Testing the Preprocessor
 	 * @param args Ignored Arguments
 	 */
 	public static void main(String[] args) throws Exception {
+		// Test Forge
 		GradleSubproject mc12 = new GradleSubproject(new File("subprojects/1.12.2/"), GradleDownloader.getGradleVersion("4.10.3"));
 		GradleSubproject mc11 = new GradleSubproject(new File("subprojects/1.11.2/"), GradleDownloader.getGradleVersion("4.10.3"));
 		GradleSubproject mc10 = new GradleSubproject(new File("subprojects/1.10.2/"), GradleDownloader.getGradleVersion("4.10.3"));
 		GradleSubproject mc9 = new GradleSubproject(new File("subprojects/1.9.4/"), GradleDownloader.getGradleVersion("4.10.3"));
 		GradleSubproject mc8 = new GradleSubproject(new File("subprojects/1.8.9/"), GradleDownloader.getGradleVersion("4.10.3"));
-		ForgeSetup.setupForge(mc8, Forge.MC1_8_9, Mappings.STABLE, "22", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod");
-		ForgeSetup.setupForge(mc9, Forge.MC1_9_4, Mappings.SNAPSHOT, "20160518", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod");
-		ForgeSetup.setupForge(mc10, Forge.MC1_10_2, Mappings.SNAPSHOT, "20161111", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod");
-		ForgeSetup.setupForge(mc11, Forge.MC1_11_2, Mappings.SNAPSHOT, "20161220", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod");
-		ForgeSetup.setupForge(mc12, Forge.MC1_12_2, Mappings.SNAPSHOT, "20171003", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod");
+		ForgeSetup.setupForge(mc8, Forge.MC1_8_9, Mappings.STABLE, "22", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod", JAVA8);
+		ForgeSetup.setupForge(mc9, Forge.MC1_9_4, Mappings.SNAPSHOT, "20160518", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod", JAVA8);
+		ForgeSetup.setupForge(mc10, Forge.MC1_10_2, Mappings.SNAPSHOT, "20161111", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod", JAVA8);
+		ForgeSetup.setupForge(mc11, Forge.MC1_11_2, Mappings.SNAPSHOT, "20161220", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod", JAVA8);
+		ForgeSetup.setupForge(mc12, Forge.MC1_12_2, Mappings.SNAPSHOT, "20171003", "lotas", "LoTAS", "de.pfannekuchen.lotas", "Pfannekuchen", "1.0.0", "Low Optimization Tool Assisted Speedrun Mod", JAVA8);
 		mc12.executeTaskAfter("build");
 		mc11.executeTaskAfter("build");
 		mc10.executeTaskAfter("build");
 		mc9.executeTaskAfter("build");
 		mc8.executeTaskAfter("build");
+		// Test Fabric
+		GradleSubproject mc14 = new GradleSubproject(new File("subprojects/1.14.4/"), GradleDownloader.getGradleVersion("7.1"));
+		GradleSubproject mc15 = new GradleSubproject(new File("subprojects/1.15.2/"), GradleDownloader.getGradleVersion("7.1"));
+		GradleSubproject mc16 = new GradleSubproject(new File("subprojects/1.16.1/"), GradleDownloader.getGradleVersion("7.1"));
+		GradleSubproject mc16_2 = new GradleSubproject(new File("subprojects/1.16.5/"), GradleDownloader.getGradleVersion("7.1"));
+		GradleSubproject mc17 = new GradleSubproject(new File("subprojects/1.17/"), GradleDownloader.getGradleVersion("7.1"));
+		FabricSetup.setupFabric(mc17, Fabric.MC1_17, "1.17+build.13", "LoTAS", "de.pfannekuchen.lotas", "1.0.0", "0.11.6", "0.35.2+17", JAVA16);
+		FabricSetup.setupFabric(mc16_2, Fabric.MC1_16_5, "1.16.5+build.10", "LoTAS", "de.pfannekuchen.lotas", "1.0.0", "0.11.6", "0.35.1+1.16", JAVA16);
+		FabricSetup.setupFabric(mc16, Fabric.MC1_16_1, "1.16.1+build.21", "LoTAS", "de.pfannekuchen.lotas", "1.0.0", "0.11.6", "0.18.0+build.387-1.16.1", JAVA16);
+		FabricSetup.setupFabric(mc15, Fabric.MC1_15_2, "1.15.2+build.17", "LoTAS", "de.pfannekuchen.lotas", "1.0.0", "0.11.6", "0.28.5+1.15", JAVA16);
+		FabricSetup.setupFabric(mc14, Fabric.MC1_14_4, "1.14.4+build.18", "LoTAS", "de.pfannekuchen.lotas", "1.0.0", "0.11.6", "0.28.5+1.14", JAVA16);
+		mc17.executeTaskAfter("remapJar");
+		mc16_2.executeTaskAfter("remapJar");
+		mc16.executeTaskAfter("remapJar");
+		mc15.executeTaskAfter("remapJar");
+		mc14.executeTaskAfter("remapJar");
 	}
 	
 }
--- src/main/java/de/pfannekuchen/preprocessor/loader/FabricSetup.java
+++ src/main/java/de/pfannekuchen/preprocessor/loader/FabricSetup.java
@@ -1,4 +1,4 @@
-package de.pfannekuchen.preprocessor.forge;
+package de.pfannekuchen.preprocessor.loader;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,37 +13,28 @@ import java.nio.file.StandardCopyOption;
 import de.pfannekuchen.preprocessor.gradle.GradleSubproject;
 
 /**
- * Forge Environment Preparer
+ * Fabric Environment Preparer
  * @author Pancake
  */
-public class ForgeSetup {
+public class FabricSetup {
 	
 	/**
-	 * Forge Versions
+	 * Fabric Minecraft Versions
 	 * @author Pancake
 	 */
-	public static enum Forge {
-		MC1_12_2, MC1_11_2, MC1_10_2, MC1_9_4, MC1_8_9;
+	public static enum Fabric {
+		MC1_14_4, MC1_15_2, MC1_16_1, MC1_16_5, MC1_17;
 	}
 	
 	/**
-	 * Mappings Channel
-	 * @author Pancake
-	 */
-	public static enum Mappings {
-		SNAPSHOT, STABLE
-	}
-	
-	/**
-	 * Sets up a Forge Environment
+	 * Sets up a Fabric Environment
 	 * @param directory Gradle Subproject
-	 * @param version Forge Version
-	 * @param channel Mappings Channel
+	 * @param version Fabric MC Version
 	 * @param mappings Mappings Version
 	 * @throws IOException Throws whenever Files couldn't be created
 	 * @throws MalformedURLException Throws whenever an Invalid Version is being used
 	 */
-	public static void setupForge(GradleSubproject directory, Forge version, Mappings channel, String mappings, String modid, String modname, String modgroup, String modauthor, String modversion, String moddescription, String... javaHome) throws MalformedURLException, IOException {
+	public static void setupFabric(GradleSubproject directory, Fabric version, String yarn, String modname, String modgroup, String modversion, String loaderversion, String fabricapiversion, String... javaHome) throws MalformedURLException, IOException {
 		// Remove Build/Bin Folder
 		new File(directory.getLocation(), "bin").delete();
 		new File(directory.getLocation(), "build").delete();
@@ -52,41 +43,27 @@ public class ForgeSetup {
 		new File(directory.getLocation(), "src/main/resources").mkdirs();
 		new File(directory.getLocation(), "gradle.properties").createNewFile();
 		Files.copy(new URL("http://mgnet.work/cfg/" + version.toString()).openStream(), new File(directory.getLocation(), "build.gradle").toPath(), StandardCopyOption.REPLACE_EXISTING);
+		Files.copy(new URL("http://mgnet.work/cfg/fabric.gradle").openStream(), new File(directory.getLocation(), "settings.gradle").toPath(), StandardCopyOption.REPLACE_EXISTING);
 		// Download gradle.properties, but change values in it
 		BufferedReader reader = new BufferedReader(new InputStreamReader(new URL("http://mgnet.work/cfg/" + version.toString() + ".properties").openStream()));
 		PrintWriter writer = new PrintWriter(new File(directory.getLocation(), "gradle.properties"));
 		String _line;
-		String forgeVersion = "%MC%-%FORGE%";
 		while ((_line = reader.readLine()) != null) {
-			if (_line.startsWith("minecraft_version")) forgeVersion = forgeVersion.replaceFirst("%MC%", _line.split("=")[1]);
-			if (_line.startsWith("forge_version")) forgeVersion = forgeVersion.replaceFirst("%FORGE%", _line.split("=")[1]);
 			writer.println(_line
-				.replaceAll("%MODID%", modid)
 				.replaceAll("%MODNAME%", modname)
 				.replaceAll("%MODGROUP%", modgroup)
-				.replaceAll("%MODAUTHOR%", modauthor)
 				.replaceAll("%MODVERSION%", modversion)
-				.replaceAll("%MODDESCRIPTION%", moddescription)
-				.replaceAll("%MAPPINGSCHANNEL%", channel.toString().toLowerCase())
-				.replaceAll("%MAPPINGSVERSION%", mappings)
+				.replaceAll("%MAPPINGSVERSION%", yarn)
+				.replaceAll("%FABRICAPI%", fabricapiversion)
+				.replaceAll("%LOADER%", loaderversion)
 			);
 		}
 		writer.println("org.gradle.jvmargs=-Xmx1250M");
 		writer.println("org.gradle.daemon=false");
-		if (javaHome.length == 1) writer.println("org.gradle.java.home=" + javaHome);
+		if (javaHome.length == 1) writer.println("org.gradle.java.home=" + javaHome[0]);
 		writer.close();
 		reader.close();
-		// Check whether SetupDecompWorkspace has been executed yet
-		if (!new File(System.getProperty("user.home") + "/.gradle/caches/minecraft/net/minecraftforge/forge/" + forgeVersion + "/" + channel.name().toLowerCase() + "/" + mappings + "/").exists()) {
-			System.out.println("SetupDecompWorkspace hasn't been executed yet. Running in the background");
-			new Thread(() -> {
-				try {
-					directory.executeTask("setupDecompWorkspace");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}).start();
-		}
+		// Note: you don't have to run genSources ever. <3 Fabric
 	}
 	
 }
